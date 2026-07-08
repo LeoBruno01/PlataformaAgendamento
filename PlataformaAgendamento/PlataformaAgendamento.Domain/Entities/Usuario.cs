17d2f7dfd4e6604d95078e695b56fc73b1fb2c42
LeoBruno01 <leozinhobatistta@gmail.com>
2026-07-07 22:51:51 -0300

    feat: criando a estrutura do banco e aplicando migration na tabela Usuario

---
+++ PlataformaAgendamento/PlataformaAgendamento.Domain/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlataformaAgendamento.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ PlataformaAgendamento/PlataformaAgendamento.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlataformaAgendamento.Domain.Entities;
+
+namespace PlataformaAgendamento.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    // aq avisa que a classe Usuario vai virar tabela "Usuarios" no banco
+    public DbSet<Usuario> Usuarios { get; set; }
+}
